b294d749b98a00a1c95b6f5d9450b666fc82d500
Marcus Oettinger <Marcus@Oettinger-physics.de>
2022-08-22 16:43:37 +0200

    Update Leaflet.Marker.InfoCircle.js

--- Leaflet.Marker.InfoCircle.js
+++ Leaflet.Marker.InfoCircle.js
@@ -5,7 +5,7 @@ L.Marker.InfoCircle = L.Marker.extend({
 		 * @option text: string
 		 * The text or number to display in the circle
 		 */
-		text: "",
+		text: " ",
 
 		/**
 		 * @option radius: number
@@ -28,35 +28,34 @@ L.Marker.InfoCircle = L.Marker.extend({
 
 	},
 
-	// This replaces the _initIcon from L.Marker, which previously
-	// was adding just one icon to the marker.
-	// this._icon becomes a container for all the stacked icons.
-	_initIcon: function () {
-		var options = this.options,
-		    classToAdd = 'leaflet-zoom-' + (this._zoomAnimated ? 'animated' : 'hide'),		
-		    size = options.radius * 2,
-		    style = 'style="width: ' + size + 'px; height: ' + size + 'px; border-width: ' + borderWidth + 'px;"',
-		    iconSize = size + (opts.borderWidth * 2);
 
-		this._icon = L.divIcon({
-			html: '<span class="' + 'circle ' + options.circleClass + '" ' 
-				+ style + '>' + options.txt + '</span>',
-			className: '', iconSize: [iconSize, iconSize]
+	/**
+	  * initialize: construct a simple iconMarker
+	  */
+	initialize: function (latlng, options) {
+		L.setOptions(this, options);
+
+		var size = this.options.radius * 2,
+		    bw = this.options.borderWidth,
+		    cSize = size + (this.options.borderWidth * 2),
+		    html = '<span class="circle ' + options.circleClass
+		         + '" style="width:' + cSize + 'px; height:' + cSize + 'px; borderwidth:' 
+		         + bw + 'px">' + this.options.text +'</span>';
+	    
+		this._latlng = L.latLng(latlng);
+		
+		icon = L.divIcon({
+    			html: html,
+    			className: '',  //'circle ' + options.circleClass,
+    			iconSize: [cSize, cSize]
   		});
-	}
+  		this.setIcon(icon);
+	},
+
 
 });
 
 
-
-L.marker.InfoCircle = function(latlng, opts) {
-	var marker = L.marker(
-		latng,
-		opts
-/* {
-		icon: icon
-	}*/
-	);
-
-	return new L.Marker.InfoCircle(latlng, opts);
+L.marker.InfoCircle = function(latlng, options) {
+	return new Marker.InfoCircle( latlng, options );
 }
